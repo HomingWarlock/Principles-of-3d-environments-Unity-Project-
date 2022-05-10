29a7b1b6e02d8c9a6afc171de57706c63e0ca63d
Michael <michael6.robinson@live.uwe.ac.uk>
2022-05-10 15:42:49 +0100

    Snowball Disater
    Snowball Disater

---
+++ Unity Tutorial - Base/Assets/Scripts/Other/SnowballGrow.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowballGrow : MonoBehaviour
+{
+    void OnCollisionStay(Collision col)
+    {
+        if (col.transform.name == "Doom Mountain")
+        {
+            this.transform.localScale += new Vector3(3, 3, 3);
+        }
+    }
+}
